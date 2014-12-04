b2dbb469cdf0e69c0bea1b24733eb8f0642928b5
yoosB <soomin.yoo95@gmail.com>
2014-12-03 23:24:19 -0800

    Debugged some more ex26

--- ex26.py
+++ ex26.py
@@ -79,13 +79,13 @@ sentence = "All good things come to those who wait."
 words = break_words(sentence)
 sorted_words = sort_words(words)
 
-print print_first_word(words)
-print print_last_word(words)
-print print_first_word(sorted_words)
-print print_last_word(sorted_words)
+print_first_word(words)
+print_last_word(words)
+print_first_word(sorted_words)
+print_last_word(sorted_words)
 sorted_words = sort_sentence(sentence)
 print sorted_words
 
-print print_first_and_last(sentence)
+print_first_and_last(sentence)
 
-print print_first_and_last_sorted(sentence)
+print_first_and_last_sorted(sentence)
