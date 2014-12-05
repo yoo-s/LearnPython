23ea15434a053579cecd5695f349ef88da54a354
yoosB <soomin.yoo95@gmail.com>
2014-12-04 18:39:05 -0800

    Use for-loops and range

--- ex33.py
+++ ex33.py
@@ -1,17 +1,19 @@
 i = 0
 numbers = []
 maxnum = 5
+add = 2
 
-def numlist(letter, num, listing):
-    while letter < num:
+def numlist(letter, num, incr, listing):
+    for letter in range(0, 5):
+    # while letter < num:
         print "At the top i is %d" % letter
         listing.append(letter)
 
-        letter = letter + 1
+        # letter = letter + incr
         print "Numbers now: ", listing
         print "At the bottom i is %d" % letter
 
-numlist(i, maxnum, numbers)
+numlist(i, maxnum, add, numbers)
 
 print "The numbers: "
 
