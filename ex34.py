caa150c3c572745530a2a5b09d0421037ad93835
yoosB <soomin.yoo95@gmail.com>
2014-12-04 22:47:52 -0800

    Add ex34

---
+++ ex34.py
@@ -0,0 +1,12 @@
+'''
+1) The animal at 1 is a python
+2) The animal at 2 is the 3rd animal and is a peacock
+3) The animal at 0 is the 1st animal and is a bear
+4) The animal at 3 is a kangaroo
+5) The animal at 4 is the 5th animal and is a whale
+6) The animal at 2 is a peacock
+7) The animal at 5 is the 6th animal and is a platypus
+8) The animal at 4 is a whale
+'''
+
+animals = ['bear', 'python', 'peacock', 'kangaroo', 'whale', 'platypus']
