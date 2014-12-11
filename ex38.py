7aa7ac1a49e96e97ab29d9a6610a0984aec4f559
yoosB <soomin.yoo95@gmail.com>
2014-12-10 23:52:36 -0800

    Add comments rewording functions

--- ex38.py
+++ ex38.py
@@ -2,13 +2,13 @@ ten_things = "Apples Oranges Crows Telephone Light Sugar"
 
 print "Wait there are not 10 things in that list. Let's fix that."
 
-stuff = ten_things.split(' ')
+stuff = ten_things.split(' ') # split(ten_things, ' ')
 more_stuff = ["Day", "Night", "Song", "Frisbee", "Corn", "Banana", "Girl", "Boy"]
 
 while len(stuff) != 10:
-	next_one = more_stuff.pop()
+	next_one = more_stuff.pop() # pop(more_stuff)
 	print "Adding: ", next_one
-	stuff.append(next_one)
+	stuff.append(next_one) # append(stuff, next_one)
 	print "There are %d items now." % len(stuff)
 
 print "There we go: ", stuff
@@ -17,6 +17,6 @@ print "Let's do some things with stuff."
 
 print stuff[1]
 print stuff[-1] # whoa! fancy
-print stuff.pop()
-print ' '.join(stuff) # what? cool!
-print '#'.join(stuff[3:5]) # super stellar!
+print stuff.pop() # pop(stuff)
+print ' '.join(stuff) # what? cool! join(stuff, ' ')
+print '#'.join(stuff[3:5]) # super stellar! join(stuff[3:5], '#')
