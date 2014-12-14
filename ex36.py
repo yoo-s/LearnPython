b8790793eef11e580aec0190bc668edf246765f7
yoosB <soomin.yoo95@gmail.com>
2014-12-13 22:55:15 -0800

    Add more 'rooms'

--- ex36.py
+++ ex36.py
@@ -7,7 +7,7 @@ def start():
 	print "You need to find your kitten."
 	print "Go~~~!"
 	print " "
-	print "You stand facing the farmhouse. There's grassy fields to your left \
+	print "You stand facing north towards the farmhouse. There's grassy fields to your left \
 and a barnhouse to your right. Behind you is a stream with a small wooden \
 bridge across it, and across the stream in the distance is a white windmill."
 	print "Type 'help' for list of possible actions."
@@ -15,13 +15,13 @@ bridge across it, and across the stream in the distance is a white windmill."
 	
 	action = raw_input("> ")
 
-	if "farmhouse" in action:
+	if "farmhouse" in action or "north" in action:
 		farmhouse()
-	elif "fields" or "left" in action:
-		grassy_fields()
-	elif "barnhouse" or "right" in action:
+	elif "fields" in action or "left" in action or "west" in action:
+		cornfield()
+	elif "barnhouse" in action or "right" in action or "east" in action:
 		barnhouse()
-	elif "cross" or "stream" in action:
+	elif "cross" in action or "stream" in action or "south" in action:
 		cross_stream()
 	elif action == "help":
 		help()
@@ -31,6 +31,15 @@ bridge across it, and across the stream in the distance is a white windmill."
 def help():
 	print "You can go in directions north, south, east, and west. You can look \
 at your surroundings, read anything readable, open things, etc. for starters."
+
+def farmhouse():
+	print "You open the door and step inside the house."
+
+def cornfield():
+	print "You're standing at the edge of a large cornfield."
+
+def barnhouse():
+	print "You enter the barnhouse."
 
 def cross_stream():
 	print "You cross the little wooden bridge curving over the gently trickling \
