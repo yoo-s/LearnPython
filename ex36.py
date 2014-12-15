8f48d96dab0e5a104225692ae1db3f15d16cf3b9
yoosB <soomin.yoo95@gmail.com>
2014-12-15 15:56:46 -0800

    Edit room descriptions

--- ex36.py
+++ ex36.py
@@ -40,27 +40,39 @@ wooden bridge across it, and across the stream in the distance is a white windmi
 		print "Unfortunately, you don't know how to do said action."
 
 def help():
-	print "'n' = go north \n's' = go south \n'e' = go east \n'w' = go west"
+	print "'n' = go north into farmhouse \n's' = go south across stream \n \
+'e' = go east into barnhouse \n'w' = go west to cornfield"
+	time.sleep(2)
+	print " "
 	start()
 
 def farmhouse():
-	print "You open the door and step inside the house. It's quiet here except \
-for the soft ticking of the grandfather clock positioned at a corner."
+	print "You open the door and step inside the house. It's quiet here in \
+the living room except for the soft ticking of the grandfather clock positioned \
+at a corner. There's a door to your right leading into the hallway."
+
+	do = raw_input("> ")
 
 def cornfield():
 	print "You're at the edge of a large cornfield. A lonely \
 scarecrow stands in the middle, crows pecking at its ragged clothes. \
 You feel scared you might find zombies lurking around inside."
 
+	do = raw_input("> ")
+
 def barnhouse():
 	print "The moment you enter the barnhouse, something whooshes past your \
 face by inches. You jump back startled, and look up to see a beautiful barn \
 owl perch itself on a wooden beam near the ceiling."
+	print "You notice a dun horse in one of the stalls. It stares at you for a \
+moment before going back to eating from the tray of oats in front of it."
+
+	do = raw_input("> ")
 
 def cross_stream():
 	print "You cross the little wooden bridge curving over the gently trickling \
 stream. You're now looking at a vast grassy plain, and far in the middle of \
-the plain stands a white windmill slowly turning in the breeze."
+the plain rests a white windmill slowly turning in the breeze."
 
 	do = raw_input("> ")
 
